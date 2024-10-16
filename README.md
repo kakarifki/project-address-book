75ca456dd7acbffc3d14886f6434627a33e8a05c
M Rifki Septiawan <rifkim91@gmail.com>
2024-10-16 18:45:44 +0700

    Create Landing Page and create contacts array

---
+++ README.md
@@ -0,0 +1,1 @@
+# Project Bearmentor Month - 2
---
+++ index.js
@@ -0,0 +1,27 @@
+const contacts = [
+    {
+        id: 1,
+        fullname: 'rifki',
+        phone_number: '6289614244937',
+        email: 'rifkim91@gmail.com',
+        location: 'Setu, Bekasi',
+        age: 20,
+    },
+    {
+        id: 2,
+        fullname: 'rifki Sep',
+        phone_number: '6289614244938',
+        email: 'rifkim19@gmail.com',
+        location: 'Setu, Bekasi, Jawa Barat',
+        age: 23,
+    },
+    {
+        id: 3,
+        fullname: 'rifki SSEE',
+        phone_number: '6289614244920',
+        email: 'rifkim91@gmail.co.id',
+        location: 'Setu, Bekasi,Bekasi',
+        age: 20,
+    }
+]
+console.log(contacts)
