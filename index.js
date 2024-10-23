3e1d14bf7ea2369f97922063fecbde48ffd5d7ee
M Rifki Septiawan <rifkim91@gmail.com>
2024-10-23 19:17:23 +0700

    feat: add Function for Add Contacts and Show array added

--- index.js
+++ index.js
@@ -1,27 +1,69 @@
 const contacts = [
-    {
-        id: 1,
-        fullname: 'rifki',
-        phone_number: '6289614244937',
-        email: 'rifkim91@gmail.com',
-        location: 'Setu, Bekasi',
-        age: 20,
-    },
-    {
-        id: 2,
-        fullname: 'rifki Sep',
-        phone_number: '6289614244938',
-        email: 'rifkim19@gmail.com',
-        location: 'Setu, Bekasi, Jawa Barat',
-        age: 23,
-    },
-    {
-        id: 3,
-        fullname: 'rifki SSEE',
-        phone_number: '6289614244920',
-        email: 'rifkim91@gmail.co.id',
-        location: 'Setu, Bekasi,Bekasi',
-        age: 20,
-    }
-]
-console.log(contacts)
+  {
+    id: 1,
+    fullname: "rifki",
+    phone_number: "6289614244937",
+    email: "rifkim91@gmail.com",
+    address: "Setu, Bekasi",
+    age: 20,
+    tags: "Friends",
+    others: "",
+  },
+  {
+    id: 2,
+    fullname: "Dimas",
+    phone_number: "6289612341243",
+    email: "dimas@gmail.com",
+    address: "Jakarta Timur, DKI Jakarta",
+    age: 21,
+    tags: "Collagues",
+    others: "HRD Staff",
+  },
+  {
+    id: 3,
+    fullname: "Boy Tobing",
+    phone_number: "6281312341234",
+    email: "Boy@gmail.com",
+    address: "Bogor, Jawa Barat",
+    age: 22,
+    tags: "Family",
+    others: "Cousin",
+  },
+];
+console.log(contacts);
+
+// fungtion untuk menambahkan kontak baru 
+const addContact = (fullname, phone_number, email, address, age, tags, others) => {
+  // Cari id terakhir dalam array dan tambahkan 1
+  const lastId = contacts.length ? contacts[contacts.length - 1].id : 0;
+  const newId = lastId + 1;
+
+  // Buat objek kontak baru
+  const newContact = {
+    id: newId,
+    fullname: fullname,
+    phone_number: phone_number,
+    email: email,
+    address: address,
+    age: age,
+    tags: tags,
+    others: others || "", // fungsi OR, optional jika tidak ada data others
+  };
+
+  // Tambahkan kontak baru ke array
+  contacts.push(newContact);
+
+  // Tampilkan data terbaru
+  console.log("Kontak baru ditambahkan:", newContact);
+  console.log("Daftar kontak terbaru:", contacts);
+};
+
+// const dataBaru = 
+try {
+  addContact("Dini Nurdini", "628765432123", "dini@gmail.com", "Jombang, Jawa Timur", 25, "Friends", "Orang Jawa Timur")  
+} catch (error) {
+  console.error("Error Input Data")
+} finally {
+  console.log("Proses Add Data selesai")
+}
+
